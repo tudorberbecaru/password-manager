5753ff2cd5bc632de055705225ddddb7184c03e6
Tudor Berbecaru <tudor.berbecaru@gmail.com>
2023-11-16 14:46:47 +0200

    Add explanatory comments

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pyperclip
 import json
 import os
 
-
 EMAIL = os.environ.get('EMAIL', 'email@example.com')
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
@@ -18,28 +17,36 @@ NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def generate_password():
 
+    # Clear the password entry
     password_entry.delete(0, "end")
 
+    # Generate password components
     password_letters = [choice(LETTERS) for _ in range(randint(8, 10))]
     password_symbols = [choice(SYMBOLS) for _ in range(randint(2, 4))]
     password_numbers = [choice(NUMBERS) for _ in range(randint(2, 4))]
 
+    # Combine and shuffle password components
     password_list = password_letters + password_symbols + password_numbers
     shuffle(password_list)
 
+    # Create the password string
     password = "".join(password_list)
+
+    # Insert password into the entry and copy to clipboard
     password_entry.insert(0, password)
     pyperclip.copy(password)
-
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save_entry():
 
+    # Get data from entry fields
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
+
+    # Create a dictionary for new entry
     new_data = {
         website: {
             "username": username,
@@ -47,9 +54,11 @@ def save_entry():
         }
     }
 
+    # Check for empty fields
     if len(website) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showinfo(title="Error", message="Please make sure you haven't left any fields empty.")
     else:
+        # Confirm entry with user
         is_ok = messagebox.askokcancel(title="Confirm Entry", message=f"These are the details entered:\n\n"
                                                                       f"Website: {website}\n"
                                                                       f"Email/Username: {username}\n"
@@ -58,19 +67,22 @@ def save_entry():
 
         if is_ok:
             try:
+                # Try to open existing data file
                 with open("data.json", "r") as data_file:
                     # Reading old data
                     data = json.load(data_file)
                     # Updating old data with new data
                     data.update(new_data)
             except FileNotFoundError:
+                # If the file does not exist, create a new one
                 with open("data.json", "w") as data_file:
                     json.dump(new_data, data_file, indent=4)
             else:
+                # Save data to data.json
                 with open("data.json", "w") as data_file:
-                    # Saving updated data
                     json.dump(data, data_file, indent=4)
             finally:
+                # Clear entry fields after saving
                 website_entry.delete(0, 'end')
                 password_entry.delete(0, 'end')
 
@@ -79,36 +91,42 @@ def save_entry():
 
 
 def find_password():
+    # Get website from entry
     website = website_entry.get()
+
     try:
+        # Try to open the data file
         with open("data.json") as data_file:
             data = json.load(data_file)
     except FileNotFoundError:
+        # Display an error message if the file does not exist
         messagebox.showinfo(title="Error", message="data.json does not exist.")
     else:
+        # Check if the website is in data.json
         if website in data:
+            # Retrieve username and password and display them
             username = data[website]["username"]
             password = data[website]["password"]
             messagebox.showinfo(title="Entry Found", message=f"Email/Username: {username}\nPassword: {password}")
         else:
+            # Display a message if no entry is found for the website
             messagebox.showinfo(title="No Entry Found", message=f"No entry was found for {website}.")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-
+# Create the main window
 window = Tk()
 window.title("Password Manager")
 window.config(padx=40, pady=50)
 
 # Canvas / Logo
-
 canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(70, 100, image=logo_image)
 canvas.grid(column=1, row=0)
 
 # Labels
-
 website_label = Label(text="Website   ", font=("Courier", 12, "bold"))
 website_label.grid(column=0, row=1)
 username_label = Label(text="Email/Username   ", font=("Courier", 12, "bold"))
@@ -117,7 +135,6 @@ password_label = Label(text="Password   ", font=("Courier", 12, "bold"))
 password_label.grid(column=0, row=3)
 
 # Entries
-
 website_entry = Entry(width=43)
 website_entry.grid(column=1, row=1, sticky="EW")
 website_entry.focus()
@@ -128,7 +145,6 @@ password_entry = Entry(width=43, show="*")
 password_entry.grid(column=1, row=3)
 
 # Buttons
-
 search_button = Button(text="Search", command=find_password)
 search_button.grid(row=1, column=2, sticky="EW")
 generate_password_button = Button(text="Generate Password", command=generate_password)
@@ -136,7 +152,4 @@ generate_password_button.grid(column=2, row=3)
 add_button = Button(text="Add", command=save_entry)
 add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
 
-try:
-    window.mainloop()
-except KeyboardInterrupt:
-    pass
+window.mainloop()
